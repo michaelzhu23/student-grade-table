cab132ed126240f2f7ee0b1902e46ff66eb1c09c
Michael Zhu <mzhu23@gmail.com>
2020-03-22 13:09:29 +0000

    Instantiated new GradeForm object

--- main.js
+++ main.js
@@ -2,5 +2,7 @@ var table = document.querySelector("table");
 var gradeTable = new GradeTable(table);
 var headerElement = document.querySelector("header");
 var pageHeader = new PageHeader(headerElement);
+var formElement = document.querySelector("form");
+var gradeForm = new GradeForm(formElement);
 var app = new App(gradeTable, pageHeader);
 app.start();
