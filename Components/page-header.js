8582b69710248daf9ff2d732ea5cdb40209df991
Michael Zhu <mzhu23@gmail.com>
2020-03-22 02:50:07 +0000

    Modified updateAverage method of PageHeader class to set average grade value to the Average Grade badge in the header element

--- Components/page-header.js
+++ Components/page-header.js
@@ -3,6 +3,7 @@ class PageHeader {
     this.headerElement = headerElement;
   }
   updateAverage(newAverage) {
-    console.log(newAverage);
+    var averageGradeBadge = document.querySelector("span.badge");
+    averageGradeBadge.textContent = newAverage;
   }
 }
