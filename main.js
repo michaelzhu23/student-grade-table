fbf58e38b5fcfece2e4158eb84802f62fecaf4fe
Michael Zhu <mzhu23@gmail.com>
2020-03-22 13:16:13 +0000

    Added gradeForm as third argument to the App instantiation

--- main.js
+++ main.js
@@ -4,5 +4,5 @@ var headerElement = document.querySelector("header");
 var pageHeader = new PageHeader(headerElement);
 var formElement = document.querySelector("form");
 var gradeForm = new GradeForm(formElement);
-var app = new App(gradeTable, pageHeader);
+var app = new App(gradeTable, pageHeader, gradeForm);
 app.start();
