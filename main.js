dd7253fd036a99d5537b48944cefc8139dbfc8b7
Michael Zhu <mzhu23@gmail.com>
2020-03-22 01:16:27 +0000

    Instantiated pageHeader object from the PageHeader class passing in the argument of headerElement

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var table = document.querySelector("table");
 var gradeTable = new GradeTable(table);
+var headerElement = document.querySelector("header");
+var pageHeader = new PageHeader(headerElement);
 var app = new App(gradeTable);
 app.start();
