7e398ce711c5fc890b68da73c6a2265cb5010359
Michael Zhu <mzhu23@gmail.com>
2020-03-22 01:20:36 +0000

    Added pageHeader object as second argument of app instantiation

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ var table = document.querySelector("table");
 var gradeTable = new GradeTable(table);
 var headerElement = document.querySelector("header");
 var pageHeader = new PageHeader(headerElement);
-var app = new App(gradeTable);
+var app = new App(gradeTable, pageHeader);
 app.start();
