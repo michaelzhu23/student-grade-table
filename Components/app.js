b7f0499e653ff6eb772b61c3f7bf01863ae4197b
Michael Zhu <mzhu23@gmail.com>
2020-03-23 08:04:16 +0000

    Binded the 3 new methods of class App to 'this' object

--- Components/app.js
+++ Components/app.js
@@ -8,6 +8,9 @@ class App{
     this.createGrade = this.createGrade.bind(this);
     this.handleCreateGradeError = this.handleCreateGradeError.bind(this);
     this.handleCreateGradeSuccess = this.handleCreateGradeSuccess.bind(this);
+    this.deleteGrade = this.deleteGrade.bind(this);
+    this.handleDeleteGradeError = this.handleDeleteGradeError(this);
+    this.handleDeleteGradeSuccess = this.handleDeleteGradeSuccess(this);
   }
   handleGetGradesError(error){
     console.error(error);
